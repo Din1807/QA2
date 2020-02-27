c74b5b46334d536edbf995036a7b3daa26d5c3a4
Din <dinlolans7@inbox.lv>
2020-02-27 22:30:21 +0200

    2 hometask. Article text comparison. Comparing the number of comments.

--- src/test/java/SecondHomework.java
+++ src/test/java/SecondHomework.java
@@ -1,67 +1,101 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.List;
 
 public class SecondHomework {
     private final By ARTICLE_TITLE = By.xpath(".//h1[contains(@class, 'headline__title')]");
     private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'd-inline')]");
-    private final By ARTICLE_COMMENT_TITLE = By.xpath("(.//h1[contains(@class, 'article-title')]//a[starts-with(a,'')])[2]");
+    private final By ARTICLE_COMMENT_TITLE = By.xpath("(.//h1[contains(@class, 'article-title')])[2]");
     private final By COMMENT_BTN = By.xpath(".//a[contains(@class,'btn-comments')]");
-    private final By TITLE_AND_COMMENT = By.xpath("//span[contains(@class, 'd-block') and .//a[contains(@class, 'comment-count')]]");
+    private final By TITLE_AND_COMMENT = By.tagName("article");
     private final By MAIN_PAGE_COMMENT_COUNT = By.xpath(".//a[contains(@class, 'comment-count')]");
     private final By ARTICLE_COMMENT_COUNT = By.xpath(".//a[contains(@class, 'd-print-none')]");
     private final By ANON_COMMENT_COUNT = By.xpath("(.//span[contains(@class, 'type-cnt')])[1]");
     private final By REG_COMMENT_COUNT = By.xpath("(.//span[contains(@class, 'type-cnt')])[2]");
+    private final Logger LOGGER = LogManager.getLogger(SecondHomework.class);
+    private WebDriver driver;
 
     @Test
     public void delfiArticleTest() {
+        LOGGER.info("Setting up driver path");
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        LOGGER.info("Opening browser");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://rus.delfi.lv/");
+        LOGGER.info("Opening Delfi home page");
         List<WebElement> articles = driver.findElements(ARTICLE_TITLE);
-        for (int i = 0; i < articles.size(); i++) {
-            if (articles.get(i).getText().length() != 0) {
-                System.out.println(i + ": " + articles.get(i).getText());
-            }
-        }
-        String mainPageTitleText = articles.get(1).getText();
-        articles.get(1).click();
+        LOGGER.info("Creating articles list");
+        int articleNum=1;
+        String mainPageTitleText = articles.get(articleNum).getText();
+        LOGGER.info("Main page text: " + mainPageTitleText);
+        articles.get(articleNum).click();
         String articleTitleText = driver.findElement(ARTICLE_PAGE_TITLE).getText();
+        LOGGER.info("Article page text: " + articleTitleText);
         Assertions.assertEquals(mainPageTitleText, articleTitleText, "Wrong title on article page!");
         driver.findElement(COMMENT_BTN).click();
-        String commentTitleText = driver.findElement(ARTICLE_COMMENT_TITLE).getText();
-        Assertions.assertEquals(mainPageTitleText, commentTitleText, "Wrong title on comment page!");
-        driver.quit();
-
+        if (!driver.findElements(COMMENT_BTN).isEmpty()) {
+            driver.findElement(COMMENT_BTN).click();
+            String commentTitleText = driver.findElement(ARTICLE_COMMENT_TITLE).getText();
+            LOGGER.info("Comment page text: " + commentTitleText);
+            Assertions.assertEquals(mainPageTitleText, commentTitleText, "Wrong title on comment page!");
+        } else{
+            LOGGER.info("Comment page does not exist or locked.");
+        }
     }
 
     @Test
     public void delfiCommentTest() {
+        LOGGER.info("Setting up driver path");
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        LOGGER.info("Opening browser");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://rus.delfi.lv/");
+        LOGGER.info("Opening Delfi home page");
         List<WebElement> articles = driver.findElements(TITLE_AND_COMMENT);
-        for (int i = 0; i < articles.size(); i++) {
-            if (articles.get(i).getText().length() != 0 && articles.get(i).getText().length() > 20) {
-                System.out.println(i + ": " + articles.get(i).getText());
-            }
+        LOGGER.info("Creating articles list");
+        int articleNum = 6;
+        LOGGER.info("Article name: " + articles.get(articleNum).findElement(ARTICLE_TITLE).getText());
+        int mainPageCommentCount, articleCommentCount, anonCommentCount, regCommentCount;
+        if (!articles.get(articleNum).findElements(MAIN_PAGE_COMMENT_COUNT).isEmpty()) {
+            mainPageCommentCount = Integer.parseInt(articles.get(articleNum).findElement(MAIN_PAGE_COMMENT_COUNT).getText().substring(1, articles.get(articleNum).findElement(MAIN_PAGE_COMMENT_COUNT).getText().length() - 1));
+        } else {
+            mainPageCommentCount = 0;
         }
-        int articleNum = 3;
-        int mainPageCommentCount = Integer.parseInt(articles.get(articleNum).findElement(MAIN_PAGE_COMMENT_COUNT).getText().substring(1, articles.get(articleNum).findElement(MAIN_PAGE_COMMENT_COUNT).getText().length() - 1));
+        LOGGER.info("Main page comment count was: " + mainPageCommentCount);
         articles.get(articleNum).findElement(ARTICLE_TITLE).click();
-        int articleCommentCount = Integer.parseInt(driver.findElement(ARTICLE_COMMENT_COUNT).getText().substring(1, driver.findElement(ARTICLE_COMMENT_COUNT).getText().length() - 1));
+        if (!driver.findElements(ARTICLE_COMMENT_COUNT).isEmpty()) {
+            articleCommentCount = Integer.parseInt(driver.findElement(ARTICLE_COMMENT_COUNT).getText().substring(1, driver.findElement(ARTICLE_COMMENT_COUNT).getText().length() - 1));
+        } else {
+            articleCommentCount = 0;
+        }
+        LOGGER.info("Article page comment count was: " + articleCommentCount);
         Assertions.assertEquals(mainPageCommentCount, articleCommentCount, "Wrong comment count on article page!");
-        driver.findElement(COMMENT_BTN).click();
-        int anonCommentCount = Integer.parseInt(driver.findElement(ANON_COMMENT_COUNT).getText().substring(1, driver.findElement(ANON_COMMENT_COUNT).getText().length() - 1));
-        int regCommentCount = Integer.parseInt(driver.findElement(REG_COMMENT_COUNT).getText().substring(1, driver.findElement(REG_COMMENT_COUNT).getText().length() - 1));
-        Assertions.assertEquals(mainPageCommentCount, anonCommentCount + regCommentCount, "Wrong count on comment page!");
+        if (!driver.findElements(COMMENT_BTN).isEmpty()) {
+            driver.findElement(COMMENT_BTN).click();
+            if (!driver.findElements(ANON_COMMENT_COUNT).isEmpty()) {
+                anonCommentCount = Integer.parseInt(driver.findElement(ANON_COMMENT_COUNT).getText().substring(1, driver.findElement(ANON_COMMENT_COUNT).getText().length() - 1));
+                regCommentCount = Integer.parseInt(driver.findElement(REG_COMMENT_COUNT).getText().substring(1, driver.findElement(REG_COMMENT_COUNT).getText().length() - 1));
+                LOGGER.info("Number of comment page comments Anon " + anonCommentCount + " and Reg " + regCommentCount + " all was: " + (anonCommentCount + regCommentCount));
+                Assertions.assertEquals(mainPageCommentCount, anonCommentCount + regCommentCount - 1, "Wrong count on comment page!");
+            } else {
+                LOGGER.info("Comment page does not exist or locked.");
+            }
+        } else {
+            LOGGER.info("Comment page does not exist.");
+        }
+    }
+
+    @AfterEach
+    public void closeBrowser() {
         driver.quit();
     }
 }
